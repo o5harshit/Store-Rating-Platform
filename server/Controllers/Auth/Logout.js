e183d6fc01b4329b29eae1215c32b52b71f4e6a2
o5harshit <srivastavaharshit400@gmail.com>
2026-07-06 02:14:44 +0530

    Changed the cookie logic

--- server/Controllers/Auth/Logout.js
+++ server/Controllers/Auth/Logout.js
@@ -2,26 +2,28 @@ import pool from "../../db/db.js";
 
 export const logout = async (req, res) => {
   try {
-    const userId = req.user?.id;
 
     // Clear Access Token Cookie
-    res.cookie("accessToken", accessToken, {
+     res.clearCookie("accessToken", {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
 
     return res.status(200).json({
       success: true,
       message: "Logged out successfully.",
     });
+
   } catch (error) {
+
     console.log(error);
 
     return res.status(500).json({
       success: false,
       message: "Internal Server Error.",
     });
+
   }
 };
